3aea3cd6b2303a7e548304ca27bf71df23d12e1b
ocampott <ocampott7@gmail.com>
2022-04-18 14:07:00 -0300

    Initialized Proyects component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { AboutMe } from "./components/AboutMe";
 import { Home } from "./components/Home";
 import { Navbar } from "./components/Navbar";
+import { Proyects } from "./components/Proyects";
 import { Skills } from "./components/Skills";
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Home/>
-      <AboutMe/>
-      <Skills/>
+      <Navbar />
+      <Home />
+      <AboutMe />
+      <Skills />
+      <Proyects />
     </div>
   );
 }
---
+++ src/components/Proyects.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Proyects = () => {
+  return (
+    <div className="w-full md:h-screen bg-[#0a192f] text-gray-300">
+        <div>
+            <div>
+                <p>Proyects</p>
+                <p>Check out my proyects</p>
+            </div>
+
+
+        </div>
+    </div>
+  )
+}
