c5bfa5436895a14d70664265c2b8f61162472700
ocampott <ocampott7@gmail.com>
2022-04-18 16:31:05 -0300

    Added Getform.io endpoint

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Contact = () => {
   return (
     <div className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4">
-        <form action="" className="flex flex-col max-w-[600px] w-full">
+        <form action="https://getform.io/f/9c0b00e1-5bc6-4326-9146-07cac3251dbc" method="POST" className="flex flex-col max-w-[600px] w-full">
             <div className="pb-8">
                 <p className="text-4xl font-bold inline border-b-4 border-yellow-300 text-gray-300">Contact</p>
                 <p className="text-gray-300 py-4">Submit the form below to contact me. I'll be waiting for your message!</p>
